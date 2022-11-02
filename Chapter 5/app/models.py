43236f8532d81730c619d0e25d06ff56f04e0420
Jake Kenny <jakenny@asu.edu>
2022-11-02 11:10:19 -0700

    Chapter 5

---
+++ Chapter 5/app/models.py
@@ -0,0 +1,37 @@
+from datetime import datetime
+from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
+
+@login.user_loader
+def load_user(id):
+	return User.query.get(int(id))
+
+#Represents users
+class User(UserMixin, db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	username = db.Column(db.String(64), index = True, unique = True)
+	email = db.Column(db.String(120), index = True, unique = True)
+	password_hash = db.Column(db.String(128))
+	posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+
+	#Tells Python how to print objects of this class
+	def __repr__(self):
+		return f'<User {self.username}>'
+
+	def set_password(self, password):
+		self.password_hash = generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.password_hash, password)
+
+#Represent blog posts written by users
+class Post(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	body = db.Column(db.String(140))
+	timestamp = db.Column(db.DateTime, index = True, default = datetime.utcnow)
+	user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+	def __repr__(self):
+		return f'<Post {self.body}>'
